26d35cc75e7aca3b7fa8c206d77256708e06075d
Mariano Capurro <marianocapurro@users.noreply.github.com>
2011-08-13 18:21:15 +0000

    Added all dependencies

--- src/main/java/ar/com/bunge/sapws/client/SAPWSClient.java
+++ src/main/java/ar/com/bunge/sapws/client/SAPWSClient.java
@@ -478,7 +478,7 @@ public class SAPWSClient {
 	 * @throws Exception
 	 */
 	protected void sslProtocolInit() throws Exception {
-		CertificateUtils.validateCertificateExpiration(getKeyStore(), getKeyStorePassword(), null, "pkcs12");
+		CertificateUtils.validateCertificateExpiration(getKeyStore(), getKeyStorePassword(), null, "jks");
 		
 		HttpSecureProtocol protocolSocketFactory;
 		protocolSocketFactory = new HttpSecureProtocol();
