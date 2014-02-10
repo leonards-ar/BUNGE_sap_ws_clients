e5aae9e4bff78aed491dbd27282ed93c315d1854
Mariano Capurro <marianocapurro@users.noreply.github.com>
2014-02-10 02:00:34 +0000

    Agregado de sampleDetermination WS

--- src/main/java/ar/com/bunge/sapws/client/parser/MonsantoResponseParser.java
+++ src/main/java/ar/com/bunge/sapws/client/parser/MonsantoResponseParser.java
@@ -11,6 +11,8 @@ import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import ar.com.bunge.util.FileUtils;
+
 /**
  *
  * @author <a href="mcapurro@gmail.com">Mariano Capurro</a>
@@ -23,8 +25,10 @@ public class MonsantoResponseParser extends BaseResponseParser {
 	
 	private static final String REGISTER_WAYBILL_RESPONSE_NODE = "ns1:registerWaybillResponse";
 	private static final String TIPOS_TEST_A_REALIZAR_RESPONSE_NODE = "ns1:typeOfTestResponse";
-	private static final String STORE_WEIGHT_RESPONSE_NODE = "ns1:storeWeightResponse ";
-	private static final String STORE_TEST_RESULT_RESPONSE_NODE = "ns1:storeTestResultResponse ";
+	private static final String STORE_WEIGHT_RESPONSE_NODE = "ns1:storeWeightResponse";
+	private static final String STORE_TEST_RESULT_RESPONSE_NODE = "ns1:storeTestResultResponse";
+	private static final String SAMPLE_DETERMINATION_RESULT_RESPONSE_NODE = "ns1:sampleDeterminationResponse";
+
 	
 	/**
 	 * 
@@ -54,7 +58,7 @@ public class MonsantoResponseParser extends BaseResponseParser {
 		if(responseNode != null) {
 			LOG.debug("Parsing [" + TIPOS_TEST_A_REALIZAR_RESPONSE_NODE + "] response");
 			StringBuffer response = new StringBuffer();
-			response.append("OK" + SUCCESS_MESSAGE_SEPARATOR + LINE_SEPARATOR);
+			response.append("OK" + SUCCESS_MESSAGE_SEPARATOR);
 			
 			Node responseDataNode = getNode(responseNode, "return");
 			if (responseDataNode != null && responseDataNode.getChildNodes() != null) {
@@ -85,8 +89,36 @@ public class MonsantoResponseParser extends BaseResponseParser {
 			response.append(buildResultRow(responseNode, new String [] {"return"}));
 			return response.toString();			
 		}
-		
+
+		responseNode = getNode(doc, SAMPLE_DETERMINATION_RESULT_RESPONSE_NODE);
+		if(responseNode != null) {
+			LOG.debug("Parsing [" + SAMPLE_DETERMINATION_RESULT_RESPONSE_NODE + "] response");
+			StringBuffer response = new StringBuffer();
+			response.append("OK" + SUCCESS_MESSAGE_SEPARATOR);
+			
+			Node responseDataNode = getNode(responseNode, "samplingList");
+			if (responseDataNode != null && responseDataNode.getChildNodes() != null) {
+				Node n;
+				for (int i = 0; i < responseDataNode.getChildNodes().getLength(); i++) {
+					n = responseDataNode.getChildNodes().item(i);
+					if ("SampleDeterminationItem".equalsIgnoreCase(n.getNodeName())) {
+						response.append(LINE_SEPARATOR);
+						response.append(buildResultRow(n, new String [] {"loadIdentifier", "sampleRequired"}));
+					}
+				}
+			}			
+			return response.toString();
+		}		
 		LOG.warn("Unexpected response: [" + rawResponse + "]");
 		return rawResponse;
 	}
+	
+	public static void main(String a[]) {
+		try {
+			MonsantoResponseParser r = new MonsantoResponseParser();
+			System.out.println(r.parseResponse(FileUtils.readFile("C:\\file.txt"), null));
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}	
 }
