064718953b150d646978b3e0a11bb2c557c320d9
mariano <mariano@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2015-03-18 02:13:46 +0000

    Added support for soapaction and ignore SSL

---
+++ project/BUNGE_sap_ws_clients/trunk/src/main/java/ar/com/bunge/util/BypassTrustSSLSocketFactory.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.md file.
+ */
+
+/**
+ * 
+ */
+package ar.com.bunge.util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.SecureRandom;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author mariano
+ *
+ */
+public class BypassTrustSSLSocketFactory extends SSLSocketFactory
+{
+	private SSLSocketFactory socketFactory;
+
+	protected final Logger logger = Logger.getLogger(getClass());
+
+	public BypassTrustSSLSocketFactory()
+	{
+		try
+		{
+			SSLContext ctx = SSLContext.getInstance("TLS");
+			ctx.init(null, new TrustManager[] { new BypassTrustManager() },
+					new SecureRandom());
+			socketFactory = ctx.getSocketFactory();
+		}
+		catch (Exception ex)
+		{
+			logger.error("Could not initialize TLS socket factory.", ex);
+		}
+	}
+
+	public static SocketFactory getDefault()
+	{
+		return new BypassTrustSSLSocketFactory();
+	}
+
+	@Override
+	public String[] getDefaultCipherSuites()
+	{
+		return socketFactory.getDefaultCipherSuites();
+	}
+
+	@Override
+	public String[] getSupportedCipherSuites()
+	{
+		return socketFactory.getSupportedCipherSuites();
+	}
+
+	@Override
+	public Socket createSocket(Socket socket, String string, int i, boolean bln) throws IOException
+	{
+		return socketFactory.createSocket(socket, string, i, bln);
+	}
+
+	@Override
+	public Socket createSocket(String string, int i) throws IOException, UnknownHostException
+	{
+		return socketFactory.createSocket(string, i);
+	}
+
+	@Override
+	public Socket createSocket(String string, int i, InetAddress ia, int i1) throws IOException, UnknownHostException
+	{
+		return socketFactory.createSocket(string, i, ia, i1);
+	}
+
+	@Override
+	public Socket createSocket(InetAddress ia, int i) throws IOException
+	{
+		return socketFactory.createSocket(ia, i);
+	}
+
+	@Override
+	public Socket createSocket(InetAddress ia, int i, InetAddress ia1, int i1) throws IOException
+	{
+		return socketFactory.createSocket(ia, i, ia1, i1);
+	}
+
+	@Override
+	public Socket createSocket() throws IOException
+	{
+		return socketFactory.createSocket();
+	}
+
+}
